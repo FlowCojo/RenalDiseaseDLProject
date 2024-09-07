5c72575a44e4feeb3ca4e9d6e922a38b2224e645
FlowCojo <florincojocaru258@gmail.com>
2024-09-07 14:00:00 +0300

    prepare base model added

--- src/cnnClassifier/pipeline/stage_01_data_ingestion.py
+++ src/cnnClassifier/pipeline/stage_01_data_ingestion.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('d:\\RenalDiseaseDLProject\\src')
 from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.components.data_ingestion import DataIngestion
 from cnnClassifier import logger 
