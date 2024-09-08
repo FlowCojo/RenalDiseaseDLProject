bafc729542d081a58f3820624025348475da904e
FlowCojo <florincojocaru258@gmail.com>
2024-09-08 14:00:00 +0300

    model training added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ sys.path.append('d:\\RenalDiseaseDLProject')
 from src.cnnClassifier import logger
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-
+from src.cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -27,5 +27,14 @@ except Exception as e:
         logger.exception(e)
         raise e
 
-
+STAGE_NAME = "Model Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
